bb320f869e249d2441406ae409f86d2a517060d2
Ryan Pietro <Ryan.waweru@strathmore.edu>
2024-07-09 23:09:08 +0300

    Modify

--- index.php
+++ index.php
@@ -30,12 +30,12 @@
 </nav>
 <div class="text-box">
     <h1>ELUTHIA</h1>
-    <p>Welcome to ELUTHIA, where nature meets ,artistry to create stunning outdoor spaces. As Kenya’s premier landscaping company, ELUTHIA is dedicated to transforming ordinary landscapes into extraordinary havens of beauty and tranquility. Our mission is to bring your vision to life through innovative design, meticulous craftsmanship, and sustainable practices.</p>
+    <p>Welcome to ELUTHIA, where nature meets artistry to create stunning outdoor spaces. As Kenya’s premier landscaping company, ELUTHIA is dedicated to transforming ordinary landscapes into extraordinary havens of beauty and tranquility. Our mission is to bring your vision to life through innovative design, meticulous craftsmanship, and sustainable practices.</p>
     <a href=""class="hero-btn">Ourteam</a>
 </div>
 
 </section>
-<setion class="Ourteam">
+<section class="Ourteam">
     <h2>WHO WE ARE</h2>
     <p>ELUTHIA is a team of passionate and skilled landscape architects, horticulturists, and garden designers committed to enhancing the natural beauty of Kenya's diverse environments. With years of experience in the industry, we have honed our expertise in creating bespoke landscapes that reflect the unique tastes and preferences of our clients.</p>
     <div class="row">
@@ -45,6 +45,14 @@
     <p>- Expert Team: Our team comprises highly skilled professionals with a deep understanding of local flora and climate conditions, ensuring optimal results for your landscape.</p>
     <p>- Quality and Reliability: At ELUTHIA, we pride ourselves on delivering top-notch quality and reliable services, ensuring your complete satisfaction from start to finish.</p>
     <p>- Sustainable Practices: We prioritize sustainability in all our projects, striving to create landscapes that are not only beautiful but also environmentally responsible.</p>
+    <div class="Ourteam-col"></div>
+
+    <h4>OUR APPROACH</h4>
+    <p>At ELUTHIA, we believe in a holistic approach to landscaping. We take the time to understand the unique characteristics of your site, including soil composition, microclimate, and existing vegetation. This allows us to design and implement solutions that are tailored to the specific conditions of your property, ensuring long-lasting beauty and functionality.</p>
+    <div class="Ourteam-col">
+    </div>
+    </div>
+    
 </head>
 <body>
     <section class="header"><nav>
