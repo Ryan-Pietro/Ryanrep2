b44ebe3fca162edc3889a6bf4c540e62ffaff9b8
Ryan Pietro <Ryan.waweru@strathmore.edu>
2024-07-09 23:41:58 +0300

    Commit

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css">
-<div class="nav-links"><ul>
+</head>
+<body>
+    <section class="header">
+        <nav>
+            <a href="index.html"><img src="images/pexels-pixabay-259588.jpg"></a>
+    <div class="nav-links"><ul>
     <li><a href="index.html">Home</a></li>
     <li><a href="contactus.html">ContactUs</a></li>
     <li><a href="Aboutus.html">Aboutus</a></li>
